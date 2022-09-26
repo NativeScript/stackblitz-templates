8c5c52cf5e84633ab9c0f50f3433e26a5c1dde58
Igor Randjelovic <rigor789@gmail.com>
2022-09-26 19:23:22 +0200

    feat: add tailwind to vue template

--- apps/nativescript-starter-vue/app/components/Home.vue
+++ apps/nativescript-starter-vue/app/components/Home.vue
@@ -1,11 +1,11 @@
 <template>
   <Page>
     <ActionBar>
-      <Label text="Home"/>
+      <Label text="Home" class="font-bold text-lg"/>
     </ActionBar>
 
     <GridLayout>
-      <Label class="info" :text="message" @tap="logMessage" />
+      <Label class="text-xl align-middle text-center text-gray-500" :text="message" @tap="logMessage" />
     </GridLayout>
   </Page>
 </template>
@@ -28,10 +28,8 @@
   });
 </script>
 
-<style scoped>
-  .info {
+<style>
+  /* .info {
     font-size: 20;
-    horizontal-align: center;
-    vertical-align: center;
-  }
+  } */
 </style>
