b649f4975bda6929c6d83cde5d584d06dedc2990
Nathan Walker <walkerrunpdx@gmail.com>
2025-01-04 13:19:20 -0800

    feat: Angular 19 standalone with optional zoneless

--- apps/nativescript-starter-angular/src/app/item/item-detail.component.ts
+++ apps/nativescript-starter-angular/src/app/item/item-detail.component.ts
@@ -1,26 +1,25 @@
-import { Component, OnInit } from '@angular/core';
-import { ActivatedRoute } from '@angular/router';
-
-import { Item } from './item';
-import { ItemService } from './item.service';
+import { Component, NO_ERRORS_SCHEMA, OnInit, inject, signal } from '@angular/core'
+import { ActivatedRoute } from '@angular/router'
+import { NativeScriptCommonModule } from '@nativescript/angular'
+import { Item } from './item'
+import { ItemService } from './item.service'
 
 @Component({
-  selector: 'ns-details',
+  selector: 'ns-item-detail',
   templateUrl: './item-detail.component.html',
+  imports: [NativeScriptCommonModule],
+  schemas: [NO_ERRORS_SCHEMA],
 })
 export class ItemDetailComponent implements OnInit {
-  item: Item;
-
-  constructor(
-    private itemService: ItemService,
-    private route: ActivatedRoute
-  ) {}
+  itemService = inject(ItemService)
+  route = inject(ActivatedRoute)
+  item = signal<Item>(null)
 
   ngOnInit(): void {
-    const id = +this.route.snapshot.params.id;
-    this.item = this.itemService.getItem(id);
+    const id = +this.route.snapshot.params.id
+    this.item.set(this.itemService.getItem(id))
 
     // log the item to the console
-    console.log(this.item);
+    console.log(this.item())
   }
 }
