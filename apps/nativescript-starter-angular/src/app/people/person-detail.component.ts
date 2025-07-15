5495f6ca8f7ef658613b401f40383b1813160d62
sean kelly <skelly@munsterbovine.ie>
2025-07-16 00:52:53 +0100

    List changed to showing famous computer scientists

---
+++ apps/nativescript-starter-angular/src/app/people/person-detail.component.ts
@@ -0,0 +1,27 @@
+import { ChangeDetectionStrategy, Component, NO_ERRORS_SCHEMA, OnInit, inject, signal } from '@angular/core'
+import { ActivatedRoute } from '@angular/router'
+import { NativeScriptCommonModule } from '@nativescript/angular'
+import { Person } from './person'
+import { PersonService } from './person.service'
+
+@Component({
+  selector: 'ns-person-detail',
+  templateUrl: './person-detail.component.html',
+  imports: [NativeScriptCommonModule],
+  providers: [PersonService],
+  schemas: [NO_ERRORS_SCHEMA],
+  changeDetection: ChangeDetectionStrategy.OnPush
+})
+export class PersonDetailComponent implements OnInit {
+  personService = inject(PersonService);
+  route = inject(ActivatedRoute);
+  person = signal<Person>(null);
+
+  ngOnInit(): void {
+    const id = +this.route.snapshot.params.id;
+    this.person.set(this.personService.getPerson(id));
+
+    // log the person to the console
+    console.log(this.person());
+  }
+}
